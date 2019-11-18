05b6c4da25a7b3d5b36e512f2ff464349f3226e6
Riku Tiira <riku@tiira.net>
2019-11-18 21:10:32 +0200

    Updates

---
+++ precache-manifest.ab69ed6131f8b3efaeda62c075907963.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a82225ca76370082faa298136fb3a3c",
+    "url": "/opening-hours/index.html"
+  },
+  {
+    "revision": "52408fd59d392c45138b",
+    "url": "/opening-hours/static/css/main.92b98b23.chunk.css"
+  },
+  {
+    "revision": "96fec5eda881a57eec80",
+    "url": "/opening-hours/static/js/2.a96d471f.chunk.js"
+  },
+  {
+    "revision": "52408fd59d392c45138b",
+    "url": "/opening-hours/static/js/main.35807ac4.chunk.js"
+  },
+  {
+    "revision": "7eb864cfe830194800d1",
+    "url": "/opening-hours/static/js/runtime-main.5576313e.js"
+  },
+  {
+    "revision": "130e75e7f13f66b72ec2acde8378b661",
+    "url": "/opening-hours/static/media/time.130e75e7.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.180748be38a706bd40bacf424c99bc69.js"
+  "/opening-hours/precache-manifest.ab69ed6131f8b3efaeda62c075907963.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/opening-hours/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
